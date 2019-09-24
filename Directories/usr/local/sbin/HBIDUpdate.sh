d4a6bf3b4a2bece7e99b71c698ae05dcbd5dc72b
Steve Zingman <N4IRS@users.noreply.github.com>
2019-09-24 05:46:33 -0400

    Update HBIDUpdate.sh

--- Directories/usr/local/sbin/HBIDUpdate.sh
+++ Directories/usr/local/sbin/HBIDUpdate.sh
@@ -8,4 +8,4 @@
 
 curl -s -N https://www.radioid.net/api/dmr/user/?id=% | jq -r '.results[] | [.id, .callsign, .fname] | @csv' | sed -e 's/"//g' > /var/lib/dvswitch/subscriber_ids.csv
 
-curl -s -N https://www.radioid.net/static/rptrids_simple_cbridge.csv | sed 's/- //g' | sed 's/,.*//g' | sed -r 's/([a-zA-Z0-9]+) ([a-zA-Z0-9]+)/\2 \1/g' > /var/lib/dvswitch/peer_ids.csv
+curl -s -N https://www.radioid.net/static/cbridge_simple_rptr.csv | sed 's/- //g' | sed 's/,.*//g' | sed -r 's/([a-zA-Z0-9]+) ([a-zA-Z0-9]+)/\2 \1/g' > /var/lib/dvswitch/peer_ids.csv
